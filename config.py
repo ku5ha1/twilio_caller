016f2c380bb1af07f6f0152189d865bbfc4b5607
kushalraga <kushalraga20@gmail.com>
2025-07-10 13:08:04 +0530

    llm added

--- config.py
+++ config.py
@@ -3,11 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Remove Twilio config, add Plivo config below as needed
-PLIVO_AUTH_ID = os.getenv("PLIVO_AUTH_ID")
-PLIVO_AUTH_TOKEN = os.getenv("PLIVO_AUTH_TOKEN")
-PLIVO_PHONE_NUMBER = os.getenv("PLIVO_PHONE_NUMBER")
-
 ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 ELEVENLABS_VOICE_ID = os.getenv("ELEVENLABS_VOICE_ID")
 
