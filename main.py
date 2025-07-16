f923afe713d3f5f0c53be2127a862a6c83642245
kushalraga <kushalraga20@gmail.com>
2025-07-16 14:43:49 +0530

    switch to python 3.11.8

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from routes import call_router
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+app = FastAPI(title="Twilio Caller")
 app.mount("/media", StaticFiles(directory="media"), name="media")
 app.include_router(call_router.router)
 
